0146893ebf60dd6216b2dabb29a5b0bc3e1d8f88
jumaniyozov <jumaniyozovse@gmail.com>
2023-04-02 18:29:54 +0500

    user and password logics added

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Register from "./Register";
+import FormContainer from "./components/Forms/FormContainer";
+
+function App() {
+  return (
+    <main className="h-full">
+      <FormContainer />
+    </main>
+  );
+}
+
+export default App;
---
+++ src/Register.tsx
@@ -0,0 +1,10 @@
+import { useRef, useState } from "react";
+
+
+const Register = () => {
+  
+
+  return <div>Register</div>;
+};
+
+export default Register;
